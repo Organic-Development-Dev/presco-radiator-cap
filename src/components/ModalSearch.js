349494fad3649e568559bd591182b67f55073b89
Anh Ngo <ngovietanh1606@gmail.com>
2024-05-02 18:51:39 +0700

    Revert "update search modal"
    This reverts commit 115b4c004a0845756df97ac3efaf560ff6e84cbf.

--- src/components/ModalSearch.js
+++ src/components/ModalSearch.js
@@ -1,28 +1,20 @@
 import { Divider, Image } from 'antd';
 import Search from 'antd/es/input/Search';
 import Modal from 'antd/es/modal/Modal';
-import { useRouter } from 'next/router';
 
 function ModalSearch(props) {
   const { open, onClose } = props;
-  const router = useRouter();
   return (
     <Modal
       closeIcon={<></>}
       open={open}
       onCancel={onClose}
       footer={null}
-      title={'Search product by name'}
+      title={null}
     >
-      <Search
-        size='large'
-        placeholder='Search product'
-        onSearch={(value, _e, info) => {
-          router.push(`/search?name=${value}`);
-          onClose();
-        }}
-      />
-      {/* <div>
+      <Search placeholder='Search product' />
+      <Divider />
+      <div>
         <div className='flex gap-2 items-center px-4 pb-4'>
           <Image src='/img/product.png' width={80} />
           <div>Product name</div>
@@ -35,7 +27,7 @@ function ModalSearch(props) {
           <Image src='/img/product.png' width={80} />
           <div>Product name</div>
         </div>
-      </div> */}
+      </div>
     </Modal>
   );
 }
