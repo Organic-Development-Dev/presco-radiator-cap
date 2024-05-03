223a9e62e1234cc3a001eefee2c769334d4f584d
Anh Ngo <ngovietanh1606@gmail.com>
2024-05-04 00:25:44 +0700

    subscriber email

---
+++ pages/api/newsletter.js
@@ -0,0 +1,33 @@
+import md5 from 'md5';
+import mailchimp from '@mailchimp/mailchimp_marketing';
+// const mailchimp = require('@mailchimp/mailchimp_marketing')
+
+mailchimp.setConfig({
+  apiKey: process.env.MAIL_CHIMP_API_KEY,
+  server: 'us22',
+});
+
+export default async function handler(req, res) {
+  const email = req.body.email;
+  if (!email) {
+    res.status(404).json({ error: 'Email not found' });
+  } else {
+    const subscriber_hash = md5(email);
+    const response = await mailchimp.lists.setListMember(
+      'a50847cf45',
+      subscriber_hash,
+      {
+        email_address: email,
+        status_if_new: 'subscribed',
+      }
+    );
+
+    res.status(200).json({ status: true, email });
+  }
+}
+
+// const response = await client.lists.addListMember("a50847cf45", {
+//     email_address: "Ebony_Brekke@gmail.com",
+//     status: "pending",
+//   });
+//   console.log(response);
--- src/components/home/subscriber.js
+++ src/components/home/subscriber.js
@@ -1,30 +1,71 @@
-import { Button, Input } from 'antd';
+import { Button, Form, Input, message } from 'antd';
+import axios from 'axios';
+import { Fragment, useState } from 'react';
 
 function Subscriber() {
+  const [email, setEmail] = useState('');
+  const [messageApi, contextHolder] = message.useMessage();
+  const key = 'updatable';
+
+  const openMessage = () => {
+    messageApi.open({
+      key,
+      type: 'loading',
+      content: 'Loading...',
+    });
+    setTimeout(() => {
+      messageApi.open({
+        key,
+        type: 'success',
+        content: 'Subscribed Successfully!',
+        duration: 2,
+      });
+    }, 1000);
+  };
+
+  const handleSubmit = async (e) => {
+    const email = e.target.email.value;
+    e.preventDefault();
+
+    const { data } = await axios.post('http://localhost:3000/api/newsletter', {
+      email,
+    });
+    if (data.status) {
+      openMessage();
+      e.target.email.value = '';
+    }
+  };
   return (
-    <div id='subscribe' className='p-10'>
-      <div className='container mx-auto'>
-        <div className='font-bold text-center uppercase text-2xl text-white'>
-          SUBSCRIBE
-        </div>
-        <div className='text-white text-center font-thin pb-4'>
-          Want to stay up to date? Subscribe to our Newsletter!
-        </div>
-        <div className='flex gap-2 justify-center'>
-          <Input
-            className='placeholder-white'
-            placeholder='Email*'
-            style={{ width: 250 }}
-          />
-          <Button
-            className='font-semibold '
-            style={{ color: 'var(--primary-color)' }}
-          >
-            Subscribe
-          </Button>
+    <Fragment>
+      {contextHolder}
+      <div id='subscribe' className='p-10'>
+        <div className='container mx-auto'>
+          <div className='font-bold text-center uppercase text-2xl text-white'>
+            SUBSCRIBE
+          </div>
+          <div className='text-white text-center font-thin pb-4'>
+            Want to stay up to date? Subscribe to our Newsletter!
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className='flex gap-2 justify-center'>
+              <Input
+                className='placeholder-white'
+                placeholder='Email*'
+                style={{ width: 250 }}
+                name='email'
+              />
+              <Button
+                htmlType='submit'
+                className='font-semibold '
+                style={{ color: 'var(--primary-color)' }}
+              >
+                Subscribe
+              </Button>
+            </div>
+          </form>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
