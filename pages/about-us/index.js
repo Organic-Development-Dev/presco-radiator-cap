1b61ff38b4c35cae0bf5359091958052b5566333
Anh Ngo <ngovietanh1606@gmail.com>
2024-05-16 23:08:23 +0700

    about page data

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -1,11 +1,14 @@
 import { Breadcrumb, Col, Image, Row } from 'antd';
 import TeamMembers from '../../src/components/home/team-member';
 import CoreValue from '../../src/components/home/core-value';
+import axios from 'axios';
+import { useEffect } from 'react';
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const content =
   'Presco is certified to the standard & guidelines of ISO 9001:2015. <br /> See our current certification. <br /> Our Pnuetek FC0750 Pressure & Leak Testing Machine uses state of <br /> the art testing equipment to measure both the radiator cap <br /> pressure & valve leak to 100th of one PSI.We have a second <br /> electronic pressure testing machine, our J.M.Bodley tester, with a <br /> comprehensive range of compatible test pots, for our range of <br /> expansion tank pressure caps.Presco follows extensive testing <br /> procedures for all its clients, and for some, all radiator caps are <br /> 100% pressure and leak tested. <br /> Presco has recently introduced a new water testing facility, for <br /> further extensive testing of our metal & expansion tank caps.';
 
-function Index() {
+function Index({ data }) {
   return (
     <div id='about-us'>
       <div style={{ backgroundColor: '#F6F6F6' }}>
@@ -30,6 +33,8 @@ function Index() {
               <Image
                 preview={false}
                 src='/img/about-us.png'
+                width={480}
+                height={357}
                 objectFit='contain'
               />
             </div>
@@ -129,8 +134,21 @@ function Index() {
       </div> */}
       <CoreValue title='Our Services' />
       <TeamMembers />
+      <div dangerouslySetInnerHTML={{ __html: data?.content?.rendered }} />
     </div>
   );
 }
 
 export default Index;
+
+export async function getStaticProps(context) {
+  const { data } = await axios.get(`http://localhost:3000/api/pages/1195`);
+
+  if (data) {
+    return {
+      props: {
+        data,
+      },
+    };
+  }
+}
