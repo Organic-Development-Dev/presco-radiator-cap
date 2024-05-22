2b4815ebf248754b08aa6b3ff1844b83e9e59050
Anh Ngo <ngovietanh1606@gmail.com>
2024-05-22 23:35:27 +0700

    fix search result

--- pages/search/index.js
+++ pages/search/index.js
@@ -24,7 +24,7 @@ function Index({ products, search }) {
 
   return (
       <>
-        <SearchPage products={currentProducts} search={search} />
+        <SearchPage products={products} search={search} />
         <Pagination
             current={currentPage}
             total={dataProducts.length}
