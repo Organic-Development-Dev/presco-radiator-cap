69ad5d18c5ef2b1c7c7972e5e432110a5e0edb54
flipflopcod3r <paul.m.cartwright1981@gmail.com>
2024-09-11 12:19:52 +0100

    update news
    removing description

--- pages/news/index.js
+++ pages/news/index.js
@@ -6,8 +6,7 @@ import client from '../../src/components/ApolloClient';
 import {DEFAULT_PRODUCT_HOME_IMG_URL} from "../../src/constants/urls";
 
 const des =
-  'Presco is certified to the standard & guidelines of ISO 9001:2015. See our current certification. Our Pnuetek FC0750 Pressure & Leak Testing Machine uses state of the art testing equipment to measure both the radiator cap pressure & valve leak to 100th of one PSI.We have a second electronic pressure testing machine, our J.M.Bodley tester, with a comprehensive range of compatible test pots, for our range of expansion tank pressure caps.Presco follows extensive testing procedures for all its clients, and for some, all radiator caps are 100% pressure and leak tested. Presco has recently introduced a new water testing facility, for further extensive testing of our metal & expansion tank caps. Using these machines Presco is able to provide the customer with detailed testing results so they may be confident that the supplied product is of perfect quality.This will be achieved through continued improvement and investment in new technology, machinery, training and at all times following the policies set down and procedures laid out in our quality manual and in accordance with the ANSI SAE J164 international standards.';
-
+  
 function Index(props) {
   const router = useRouter();
   const { blogs } = props;
